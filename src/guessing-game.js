67ea463bdd5e322b93244b9c9f7cc784869e53d6
Uylia <uyliauylia@yandex.ru>
2022-08-16 20:58:28 +0400

    add solutions

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(min, max, mid) {
+      this.min = min;
+      this.max = max;
+      this.mid = mid;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
     guess() {
-
+      this.mid = this.min + (this.max-this.min) / 2;
+      return Math.ceil(this.mid);
     }
-
     lower() {
-
+      this.max = Math.ceil(this.mid);
     }
-
     greater() {
-
+      this.min = Math.ceil(this.mid);
     }
 }
 
